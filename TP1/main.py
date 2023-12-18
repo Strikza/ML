c86ca8eb508fcb5912286fbdd1874f97202830fa
Samuel Goubeau <sams86240@gmail.com>
2023-12-18 18:37:27 +0100

    Add a main.py to start all files && Modifications of the file tree

--- TP1/main.py
+++ TP1/main.py
@@ -1,23 +1,23 @@
 print("\n================================== EXERCICE 1 ===================================")
-with open("./src/ex1.py", encoding='iso-8859-1') as f:
+with open("./TP1/src/ex1.py", encoding='iso-8859-1') as f:
     exec(f.read())
 
 print("\n================================== EXERCICE 2.1 =================================")
-with open("./src/ex2_1.py", encoding='iso-8859-1') as f:
+with open("./TP1/src/ex2_1.py", encoding='iso-8859-1') as f:
     exec(f.read())
 
 print("\n================================== EXERCICE 2.2 =================================")
-with open("./src/ex2_2.py", encoding='iso-8859-1') as f:
+with open("./TP1/src/ex2_2.py", encoding='iso-8859-1') as f:
     exec(f.read())
 
 print("\n================================== EXERCICE 2.3 =================================")
-with open("./src/ex2_3.py", encoding='iso-8859-1') as f:
+with open("./TP1/src/ex2_3.py", encoding='iso-8859-1') as f:
     exec(f.read())
 
 print("\n================================== EXERCICE 2.4 =================================")
-with open("./src/ex2_4.py", encoding='iso-8859-1') as f:
+with open("./TP1/src/ex2_4.py", encoding='iso-8859-1') as f:
     exec(f.read())
 
 print("\n================================== EXERCICE 3 ===================================")
-with open("./src/ex3.py", encoding='iso-8859-1') as f:
+with open("./TP1/src/ex3.py", encoding='iso-8859-1') as f:
     exec(f.read())
